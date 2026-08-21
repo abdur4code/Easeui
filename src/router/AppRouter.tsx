9b1617f8ce9011e32dfb493e24f01605a7e436cc
abdur4code <abdurrahim0101001@gmail.com>
2026-08-22 02:27:07 +0530

    feat: add Layout component and LayoutPage with routing integration

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -8,6 +8,8 @@ import ModalPage from "@/pages/components/ModalPage";
 import InputPage from "@/pages/components/InputPage";
 import NavbarPage from "@/pages/components/NavbarPage";
 import TooltipPage from "@/pages/components/TooltipPage";
+import CarouselPage from "@/pages/components/CarouselPage";
+import LayoutPage from "@/pages/components/LayoutPage";
 
 type Props = {};
 
@@ -48,6 +50,14 @@ const AppRouter = ({}: Props) => {
             {
               path: "tooltip",
               element: <TooltipPage />
+            },
+            {
+              path: "carousel",
+              element: <CarouselPage />
+            },
+            {
+              path: "layout",
+              element: <LayoutPage />
             }
           ],
         },
